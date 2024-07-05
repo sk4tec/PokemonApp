1b1286a88501c8cd9374ac9781e4fc53aabdf04a
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-07-05 11:07:51 +0100

    Initial Commit

---
+++ PokemonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonApp
+//
+//  Created by Sunjay Kalsi on 05/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Sunjay Kalsi on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
