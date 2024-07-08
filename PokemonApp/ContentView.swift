6a94272b4590afd4fb25787fce16acfc01c80d2d
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-07-08 16:27:18 +0100

    Initial check in - create network to get Pokemons

--- PokemonApp/ContentView.swift
+++ PokemonApp/ContentView.swift
@@ -7,7 +7,24 @@
 
 import SwiftUI
 
+enum ApiError: Error {
+    case invalidUrl
+    case invalidApi
+    case invalidResponse
+}
+
+struct PokemonListResponse: Codable {
+    let results: [PokemonEntry]
+}
+
+// Define the structure for each Pokemon entry
+struct PokemonEntry: Codable {
+    let name: String
+    let url: String
+}
+
 struct ContentView: View {
+    @State var pokemons = []
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +33,33 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .task {
+            do {
+                let pokemons = try await fetchPokemonList()
+                print(pokemons)
+            } catch {
+                print("Error /(error)")
+            }
+        }
+    }
+
+
+    func fetchPokemonList() async throws -> PokemonListResponse {
+        let urlString = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
+        guard let url = URL(string: urlString) else { throw ApiError.invalidUrl }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw ApiError.invalidResponse }
+
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(PokemonListResponse.self, from: data)
+        } catch {
+            print("error \(error)")
+            throw ApiError.invalidResponse
+        }
     }
 }
 
